c8e0e0b8a03e6992cc9ca1ffcd05d52bb9b194b4
Raghunath-Patra <raghunathpatra909@gmail.com>
2024-08-15 20:36:10 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://bingogame-backend.onrender.com",
   },
 });
 
